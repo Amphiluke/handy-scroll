a4fa2b5584dc30a9f5d7acb472337fef8ccaffd4
Amphiluke <amphiluke@gmail.com>
2022-04-09 15:17:17 +0700

    Update dev dependencies

--- dist/handy-scroll.js
+++ dist/handy-scroll.js
@@ -1,7 +1,7 @@
 /*!
 handy-scroll v1.1.0
 https://amphiluke.github.io/handy-scroll/
-(c) 2021 Amphiluke
+(c) 2022 Amphiluke
 */
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
